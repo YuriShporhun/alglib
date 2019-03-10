48211556aa5444698162c31e96e4e9d48abdc25a
Юрий <YuriPCLive@live.ru>
2019-03-11 00:20:39 +0300

    add mocha to dev;  stringMatcher.linear bench on small, medium and large string

--- benchmark/stringMatcher.perf.js
+++ benchmark/stringMatcher.perf.js
@@ -2,15 +2,27 @@ var Benchmark = require('benchmark');
 var suite = new Benchmark.Suite;
 var StringMatcher = require('../src/strings/stringMatcher.js')
 
-var smalllength = new StringMatcher('abcuuuabcuuuabcuuuabcuuu');
+var fs = require('fs');
 
-suite.add('linear#smalllength', function() {
-  smalllength.linear("abc");
+var longString = fs.readFileSync( __dirname + '/benchdata/longstring.txt').toString();
+var hugeString = fs.readFileSync( __dirname + '/benchdata/hugestring.txt').toString();
+
+var longLength = new StringMatcher(longString);
+var smallLength = new StringMatcher('abcuuuabcuuuabcuuuabcuuu');
+var hugeLength = new StringMatcher(hugeString)
+
+suite
+  .add('linear#smalllength < 100 chars              ', function() {
+    smallLength.linear("abc");
+}).add('linear#longlength > 8000 chars; < 9000 chars', function() {
+    longLength.linear("aaa");
+}).add('linear#hugelength ~600000 chars             ', function() {
+    hugeLength.linear("aaa");
 })
 .on('cycle', function(event) {
-  console.log(String(event.target));
+console.log(String(event.target));
 })
 .on('complete', function() {
-  console.log('Fastest is ' + this.filter('fastest').map('name'));
+console.log('Fastest is ' + this.filter('fastest').map('name'));
 })
 .run({ 'async': true });
